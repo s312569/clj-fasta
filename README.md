bb6c9290c7ce42c44dd09d90d0b2b65793f842b1
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-04-14 19:41:15 +1000

    Added biodb dependency

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Parser for sequences in FASTA format.
 Import from clojars:
 
 ```clojure
-[clj-fasta "0.1.5"]
+[clj-fasta "0.1.6"]
 ```
 
 Import into namespace:
--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject clj-fasta "0.1.5"
+(defproject clj-fasta "0.1.6"
   :description "Parser for sequences in FASTA format."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [fs "1.3.3"]
-                 [org.clojure/tools.nrepl "0.2.12"]]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [biodb "0.1.1"]]
   :repl-options {:init (set! *print-length* 100)}
   :jvm-opts ["-Xmx1000M"])
