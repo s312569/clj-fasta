b89cede473d3cf4b1c89051061cf4ff87f23ccc9
Jason Mulvenna <jason.mulvenna@gmail.com>
2017-04-15 10:41:32 +1000

    Updating dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/clojurescript "1.9.521"]
                  [fs "1.3.3"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [biodb "0.2.2"]]
+                 [org.clojure/tools.nrepl "0.2.13"]
+                 [biodb "0.2.4"]]
   :plugins [[lein-cljsbuild "1.1.3"]]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/cljc"]
